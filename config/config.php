cb7f5e0280bb03d32a5ef5fe9cb67213353d1544
Stephen Beck <beckstep@amazon.com>
2017-09-11 19:19:27 +0000

    Updates to config.php and database.php for prod branch

--- config/config.php
+++ config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://54.82.241.53/uvw';
+$config['base_url'] = 'http://www.rstephenbeck.club/uvw';
 /*
 |--------------------------------------------------------------------------
 | Index File
